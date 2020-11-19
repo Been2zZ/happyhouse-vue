fab699ef6685ef3691a0d508d801631d13b18fb7
Hwan <choihwan2@naver.com>
2020-11-19 19:44:29 +0900

    Update AptSeries

--- src/views/HouseSearch.vue
+++ src/views/HouseSearch.vue
@@ -1,25 +1,24 @@
 <template>
   <b-container class="bv-example-row">
-    <br>
-    <map-search-bar @search-text="searchText" />
-    <br>
+    <br />
+    <map-search-bar @search-text="searchText" @houseList="searchDong" />
+    <br />
     <b-row>
       <b-col cols="8">
-        <house-map/>
+        <house-map />
       </b-col>
       <b-col cols="4">
-        <apt-list :aptlist="apts" @select-apt = "selectApt" />
+        <apt-list :aptlist="apts" @select-apt="selectApt" />
       </b-col>
     </b-row>
   </b-container>
 </template>
 
-
 <script>
 import MapSearchBar from '@/components/MapSearchBar.vue';
-import http from "../http-common";
-import HouseMap from '../components/HouseMap.vue';
-import AptList from '../components/AptList.vue';
+import http from '../http-common';
+import HouseMap from '@/components/HouseMap.vue';
+import AptList from '@/components/AptList.vue';
 
 export default {
   name: 'Board',
@@ -28,43 +27,49 @@ export default {
     HouseMap,
     AptList,
   },
+  props: {
+    houses: Array,
+  },
   data() {
     return {
-      boards : [],
-      apts : [],
-      selectedApt : '',
+      boards: [],
+      apts: [],
+      selectedApt: '',
     };
   },
 
-  created : function(){
-    // 후에 응답받는걸로 변경해야함.
-    http
-    .get("/map/house/광장동")
-    .then((response) => {
-      this.apts = response.data;
-      console.log(response);
-    })
-    .catch ((error) => {
-      console.log(error);
-    }) 
-  },
+  // created: function() {
+  //   // 후에 응답받는걸로 변경해야함.
+  //   http
+  //     .get('/map/house/광장동')
+  //     .then((response) => {
+  //       this.apts = response.data;
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
   methods: {
-    searchText : function(text){
+    searchText: function(text) {
       console.log('>>>>' + text);
       http
-      .get("/board/" + text)
-      // .then(response => {
-      //   boards = response.data;
-      //   console.log(this.boards);
-      // })
-      .catch((error) => {
-        console.log(error);
-      })
+        .get('/board/' + text)
+        // .then(response => {
+        //   boards = response.data;
+        //   console.log(this.boards);
+        // })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    selectApt : function(apt) {
+    selectApt: function(apt) {
       this.selectedApt = apt;
     },
-
+    searchDong: function(houses) {
+      this.apts = houses;
+      console.log(this.apts);
+    },
   },
 };
 </script>
