828f86a8e4d08a50eb9774b708fd442b51c9b0e1
Hwan <choihwan2@naver.com>
2020-11-19 16:50:02 +0900

    Add AptList and HouseMap.vue

---
+++ src/components/AptListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card" @click="selectApt">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ apt.houseName }}
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+        iaculis mauris.
+        <a href="#">@bulmaio</a>. <a href="#">#css</a>
+        <a href="#">#responsive</a>
+        <br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AptListItem',
+  props: {
+    apt: Object,
+  },
+  methods: {
+    selectApt: function() {
+      this.$emit('select-apt', this.apt);
+    },
+  },
+};
+</script>
+
+<style>
+.img-list {
+  width: 50px;
+}
+</style>
--- src/views/HouseSearch.vue
+++ src/views/HouseSearch.vue
@@ -3,6 +3,14 @@
     <br>
     <map-search-bar @search-text="searchText" />
     <br>
+    <b-row>
+      <b-col cols="8">
+        <house-map/>
+      </b-col>
+      <b-col cols="4">
+        <apt-list :aptlist="apts" @select-apt = "selectApt" />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -10,16 +18,35 @@
 <script>
 import MapSearchBar from '@/components/MapSearchBar.vue';
 import http from "../http-common";
+import HouseMap from '../components/HouseMap.vue';
+import AptList from '../components/AptList.vue';
 
 export default {
   name: 'Board',
   components: {
     MapSearchBar,
+    HouseMap,
+    AptList,
   },
   data() {
     return {
       boards : [],
+      apts : [],
+      selectedApt : '',
     };
+  },
+
+  created : function(){
+    // 후에 응답받는걸로 변경해야함.
+    http
+    .get("/map/house/광장동")
+    .then((response) => {
+      this.apts = response.data;
+      console.log(response);
+    })
+    .catch ((error) => {
+      console.log(error);
+    }) 
   },
   methods: {
     searchText : function(text){
@@ -33,7 +60,11 @@ export default {
       .catch((error) => {
         console.log(error);
       })
-    }
+    },
+    selectApt : function(apt) {
+      this.selectedApt = apt;
+    },
+
   },
 };
 </script>
