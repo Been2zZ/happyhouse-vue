a52a4fa1a4c6b11fac42b76328e94c627eff8781
Hwan <choihwan2@naver.com>
2020-11-21 01:30:15 +0900

    Update LoginTokenStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,20 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 import http from '../http-common';
 
 Vue.use(Vuex);
 
-
 export default new Vuex.Store({
   state: {
-    accessToken: null,
-    userId: "",
-    userName: ""
+    accessToken: localStorage.getItem('accessToken') || null,
+    userId: localStorage.getItem('userId') || '',
+    userName: localStorage.getItem('userName') || '',
   },
   getters: {
     getAccessToken(state) {
+      // state.accessToken = localStorage.accessToken;
+      // console.log(state);
+      if (state.accessToken === 'null') return false;
       return state.accessToken;
     },
     getUserId(state) {
@@ -20,36 +22,40 @@ export default new Vuex.Store({
     },
     getUserName(state) {
       return state.userName;
-    }
+    },
   },
   mutations: {
     LOGIN(state, payload) {
-      state.accessToken = payload["auth-token"];
-      state.userId = payload["user-id"];
-      state.userName = payload["user-name"];
+      state.accessToken = payload['auth-token'];
+      state.userId = payload['user-id'];
+      state.userName = payload['user-name'];
+      localStorage.accessToken = payload['auth-token'];
+      localStorage.userId = payload['user-id'];
+      localStorage.userName = payload['user-name'];
     },
     LOGOUT(state) {
       state.accessToken = null;
-      state.userId = "";
-      state.userName = "";
-    }
+      state.userId = '';
+      state.userName = '';
+      localStorage.accessToken = null;
+      localStorage.userId = '';
+      localStorage.userName = '';
+    },
   },
   actions: {
     LOGIN(context, user) {
-      return http
-        .post(`/member/login`, user)
-        .then((response) => {
-          context.commit("LOGIN", response.data);
-          http.defaults.headers.common[
-            "auth-token"
-          ] = `${response.data["auth-token"]}`;
-          console.log(response);
-        });
+      return http.post(`/member/login`, user).then((response) => {
+        context.commit('LOGIN', response.data);
+        http.defaults.headers.common[
+          'auth-token'
+        ] = `${response.data['auth-token']}`;
+        console.log(response);
+      });
     },
     LOGOUT(context) {
-      context.commit("LOGOUT");
-      http.defaults.headers.common["auth-token"] = undefined;
-    }
+      context.commit('LOGOUT');
+      http.defaults.headers.common['auth-token'] = undefined;
+    },
   },
-  modules: {}
+  modules: {},
 });
