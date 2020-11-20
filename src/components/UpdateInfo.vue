c80bd5d90cd24bda28e89888fda941c6572dc205
정수빈 <jeongsubin@jeongsubin-ui-MacBook-Pro.local>
2020-11-21 04:40:13 +0900

    update : modify admin

--- src/components/UpdateInfo.vue
+++ src/components/UpdateInfo.vue
@@ -73,6 +73,69 @@
     <div id="_admin" v-if="user.isAdmin === 1">
       <!-- 관리자 정보 수정 화면 -->
       <strong>관리자 정보 수정 화면</strong>
+      <section>
+        <b-field label="이름">
+          <b-input name="name" v-model="name" placeholder="이름을 입력하세요."></b-input>
+        </b-field>
+
+        <b-field label="비밀번호">
+          <b-input
+            type="password"
+            value=""
+            name="pw"
+            v-model="pw"
+            placeholder="비밀번호를 입력하세요."
+            password-reveal
+          >
+          </b-input>
+        </b-field>
+
+        <b-field label="전화번호">
+          <b-input
+            type="tel"
+            name="tel"
+            v-model="phoneNum"
+            value=""
+            placeholder="전화번호를 입력하세요."
+          >
+          </b-input>
+        </b-field>
+
+        <b-field label="Email">
+          <b-input
+            type="email"
+            name="email"
+            v-model="email"
+            value=""
+            placeholder="이메일을 입력하세요."
+          >
+          </b-input>
+        </b-field>
+
+        <b-field label="주소">
+          <b-input
+            maxlength="100"
+            name="address"
+            v-model="address"
+            type="text"
+            placeholder="ex. 대전광역시"
+          ></b-input>
+        </b-field>
+
+        <b-field label="상세 주소">
+          <b-input
+            maxlength="100"
+            name="address_detail"
+            v-model="addressDetail"
+            type="textarea"
+            placeholder="ex. 유성구 봉명동"
+          ></b-input>
+        </b-field>
+
+        <b-button @click="modAdmin()" type="submit" name="button" variant="is-primary">
+          관리자 정보 수정
+        </b-button>
+      </section>
     </div>
   </section>
 </template>
@@ -143,6 +206,26 @@ export default {
             this.$router.push('/');
           } else {
             alert('회원 정보 수정에 실패 되었습니다');
+          }
+        });
+      this.submitted = true;
+    },
+    modAdmin() {
+      http
+        .post('member/modify', {
+          pw: this.pw,
+          email: this.email,
+          phoneNum: this.phoneNum,
+          address: this.address,
+          isAdmin: 1,
+          addressDetail: this.addressDetail,
+        })
+        .then((response) => {
+          if (response.data.state == 'succ') {
+            alert('관리자 정보 수정이 완료 되었습니다.');
+            this.$router.push('/');
+          } else {
+            alert('관리자 정보 수정에 실패 되었습니다');
           }
         });
       this.submitted = true;
