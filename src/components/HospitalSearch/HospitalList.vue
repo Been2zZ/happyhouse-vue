a89b6bcfa54543f8a658a2b586baf3f438761202
Been2zZ <sxxxbb@naver.com>
2020-11-23 19:51:23 +0900

    update hosital list

--- src/components/HospitalSearch/HospitalList.vue
+++ src/components/HospitalSearch/HospitalList.vue
@@ -1,18 +1,62 @@
 <template>
-  <div>
+  <section>
+    <b-table
+      :data="hospitalList"
+      ref="table"
+      paginated
+      per-page="5"
+      detailed
+      detail-key="tel"
+      :opened-detailed="defaultOpenedDetails"
+      :show-detail-icon="showDetailIcon"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+    >
+      <b-table-column field="hospitalName" label="병원 목록" sortable v-slot="props">
+        <template v-if="showDetailIcon">
+          {{ props.row.name }}
+        </template>
+        <template v-else>
+          <a @click="props.toggleDetails(props.row)">
+            {{ props.row.name }}
+          </a>
+        </template>
+      </b-table-column>
+
+      <template slot="detail" slot-scope="props">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+              <!-- <p> -->
+              <strong> 위치 : {{ props.row.address }} <br /> </strong>
+              전화번호 : {{ props.row.tel }}<br />
+              공휴일 운영시간 : {{ props.row.weekdayop == '' ? '휴무' : props.row.weekdayop }}<br />
+              <!-- </p> -->
+            </div>
+          </div>
+        </article>
+      </template>
+    </b-table>
+  </section>
+  <!-- <div>
     <hospital-item v-for="(hospital, index) in hospitalList" :key="index" :hospital="hospital" />
-  </div>
+  </div> -->
 </template>
 
 <script>
-import HospitalItem from '@/components/HospitalSearch/HospitalItem.vue';
+// import HospitalItem from '@/components/HospitalSearch/HospitalItem.vue';
 export default {
   name: 'HospitalList',
   data() {
-    return {};
+    return {
+      defaultOpenedDetails: [1],
+      showDetailIcon: true,
+    };
   },
   components: {
-    HospitalItem,
+    // HospitalItem,
   },
   props: {
     hospitalList: Array,
@@ -21,4 +65,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+span {
+  margin: auto;
+}
+</style>
