cc725c73f59e7893dc9c8995ee9479144b308505
Been2zZ <sxxxbb@naver.com>
2020-11-24 17:07:48 +0900

    update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -28,40 +28,31 @@
           </div>
         </div>
       </article>
-      <div></div>
+      <div><span> </span></div>
       <!--  -->
       <article class="tile is-child notification is-info">
         <div class="card">
           <div class="card-image">
             <figure class="image is-4by3">
               <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Placeholder image"
+                src="https://avatars3.githubusercontent.com/u/40540693?s=460&u=f5b5729616d9d94edb0c7133490f06cf1985fc24&v=4"
+                alt="수빈 image"
               />
             </figure>
           </div>
           <div class="card-content">
             <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img
-                    src="https://bulma.io/images/placeholders/96x96.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
-              </div>
               <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
+                <p class="title is-4">정수빈</p>
+                <p class="subtitle is-6">SSAFY 대전 4기</p>
               </div>
             </div>
 
             <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris.
-              <a>@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+              귀염둥이 개발자입니당<br />
+              <a target="_blank" href="https://github.com/Been2zZ">#github</a>
               <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              <time datetime="2016-1-1">3 Jan 1998</time>
             </div>
           </div>
         </div>
@@ -77,7 +68,8 @@ export default {
 
 <style scoped>
 .notification.is-info {
-  background-color: #8a3cf0;
+  /* background-color: #8a3cf0; */
+  background-color: white;
   color: #fff;
 }
 </style>
